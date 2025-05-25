2d249cf04ce2eb8ed19b292a5364fd188014763f
Moonman <seagame.std@gmail.com>
2025-05-25 22:48:50 +0700

    save

--- components/utils/input/InputGroupSlot.vue
+++ components/utils/input/InputGroupSlot.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <slot />
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts" setup></script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>
-    {{ $t("login") }}
-    {{ $t("forget-password") }}
-    <Button label="toggle dark/light" @click="darkModeStore.toggle" />
-    {{ darkModeStore.$state.isDark ? "Dark Mode" : "Light Mode" }}
+  <div
+    class="grid grid-cols-[2fr_1fr] w-screen h-screen dark:text-surface-200 dark:bg-surface-900 overflow-hidden"
+  >
+    <section>
+      <img
+        src="/images/world.jpg"
+        alt=""
+        class="w-full h-full object-cover dark:brightness-100"
+      />
+    </section>
+
+    <section class="flex flex-col items-center justify-center p-8">
+      <form @submit.prevent="">
+        <header>
+          <h1 class="text-4xl font-bold">Welcome to the Login Page</h1>
+          <p class="mt-2">Please enter your credentials to continue.</p>
+        </header>
+        <main>
+          <!-- username -->
+          <!-- password -->
+        </main>
+        <footer class="flex justify-between">
+          <Button type="button" link label="forget password ?" />
+          <Button type="submit" label="Login" />
+        </footer>
+      </form>
+    </section>
   </div>
 </template>
 
 <script lang="ts" setup>
-const darkModeStore = useDarkModeStore()
+definePageMeta({
+  layout: false,
+})
 </script>
 
 <style></style>
