eb14c54aa789d4facd0ed407e0d1eb3b18741e99
Moonman <seagame.std@gmail.com>
2025-05-25 17:17:30 +0700

    add : persistent state

--- pages/login.vue
+++ pages/login.vue
@@ -3,9 +3,14 @@
     {{ $t("login") }}
 
     {{ $t("forget-password") }}
+    {{ darkModeStore.$state.isDark ? "Dark Mode" : "Light Mode" }}
+
+    <button @click.prevent="darkModeStore.toggle">toggle</button>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const darkModeStore = useDarkModeStore()
+</script>
 
 <style></style>
