ceafe4f4c5391ab478bb9d95e808069dc7208b55
Moonman <seagame.std@gmail.com>
2026-03-07 15:46:16 +0700

    feat: add create-api-service skill and refactor API structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,9 @@
 <template>
   <div class="bg-black text-white text-3xl">
     <Icon name="mdi:home" />
-    <ul>
-      <li v-for="usr in data" :key="usr.id">{{ usr.username }}</li>
-    </ul>
   </div>
 </template>
 
-<script lang="ts" setup>
-// context
-const { $service } = useNuxtApp()
-
-const { data, pending } = await useAsyncData("get-all-user", async () => {
-  return $service.user.getAll()
-})
-</script>
+<script lang="ts" setup></script>
 
 <style></style>
---
+++ plugins/api/example/create.type.ts
@@ -0,0 +1,8 @@
+export type ExampleCreatePayload = {
+  example: string
+}
+
+export type ExampleCreateResponse = {
+  id: number
+  example: string
+}
